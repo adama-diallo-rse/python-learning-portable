123990a54e011afed6b467f9b2c9350d37fb9b7c
AdamaDiallo <adama7.diallo.pro@gmail.com>
2026-04-03 21:22:16 +0200

    Lecture 1: Deep Thought

---
+++ lecture1/interpreter.py
@@ -0,0 +1,16 @@
+x, y, z = input("Expression: ").split()
+x = int(x)
+z = int(z)
+
+if y == '+':
+    result = x + z
+    
+elif y == '-':
+    result = x - z
+   
+elif y == '*':
+    result = x * z
+   
+elif y == '/':
+     result = x / z
+   
